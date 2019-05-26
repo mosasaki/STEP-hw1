0538a33ef7d753a78ced3ffc99f04d0929afd164
mosasaki <m.sasaki261@gmail.com>
2019-05-26 16:53:57 +0900

    second commit

--- find_anagram.c
+++ find_anagram.c
@@ -1,3 +1,5 @@
+
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -9,122 +11,159 @@
 int n;
 
 typedef struct Dictionary{
-  char *original;
-  char *sorted;
+    char *original;
+    char *sorted;
 }Dictionary;
 
 Dictionary table[SIZE];
 
 
 void insert_end(char *orig, char *sort);
+void table_quicksort(Dictionary table[], int left, int right);
 void read_file(char *filename);
 void sort(char *quickorig, int left, int right);
 char *quicksort(char *orig, int left, int right);
 char *find_anagram(char *input);
 
 
+
 void insert_end(char *orig, char *sort){
-  table[n].original = orig;
-  table[n].sorted = sort;
-  n++;
+    table[n].original = orig;
+    table[n].sorted = sort;
+    n++;
 }
 
 
 void sort(char *quickorig,int left,int right){
     int pivot,i,j,tmp;
-  
+
     if(left < right){
-      pivot = quickorig[(left+right)/2];
-      i = left;
-      j = right+1;
-      while(i <= j){
-        
-	while(quickorig[i] < pivot){
-	  i++;
-	}
-	while(quickorig[j] > pivot){
-	  j--;
-	}
-	if(i <= j ){
-	  tmp = quickorig[i];
-	  quickorig[i] = quickorig[j];
-	  quickorig[j] = tmp;
-	  i++;
-	  j--;
-	}
-      }
-      sort(quickorig, left, j);
-      sort(quickorig, i, right);
+        pivot = quickorig[left];
+        i = left;
+        j = right;
+        while(i <j ){
+
+            while(quickorig[i] < pivot){
+                i++;
+            }
+            while(quickorig[j] > pivot){
+                j--;
+            }
+            if(i <= j ){
+                tmp = quickorig[i];
+                quickorig[i] = quickorig[j];
+                quickorig[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+        sort(quickorig, left, j);
+        sort(quickorig, i, right);
     }
 }
 
 char *quicksort(char *orig,int left,int right){
-  char *quickorig;
-  quickorig = (char *) malloc (LINE_LENGTH);
-  strcpy(quickorig,orig);
-  
+    char *quickorig;
+    quickorig = (char *) malloc (LINE_LENGTH);
+    strcpy(quickorig,orig);
 
-  sort(quickorig,left,right);
-  return(quickorig);
+
+    sort(quickorig,left,right);
+    return(quickorig);
 
 }
-  
-   
 
+
+void table_quicksort(Dictionary table[], int left, int right) {
+
+int i,j;
+char *pivot;
+Dictionary tmp;
+
+if(left < right) {
+    i = left;
+    j = right;
+    pivot = table[(left + right) / 2].sorted;
+
+    while(i < j){
+        while(strcasecmp(table[i].sorted, pivot)<0){
+            i++;
+        }
+        while(strcasecmp(table[j].sorted, pivot) > 0){
+            j--;
+        }
+        if(i <= j){
+            tmp.original = table[i].original;
+            tmp.sorted = table[i].sorted;
+            table[i].original = table[j].original;
+            table[i].sorted = table[j].sorted;
+            table[j].original = tmp.original;
+            table[j].sorted = tmp.sorted;
+            i++;
+            j--;
+        }
+    }
+    table_quicksort(table,left,j);
+    table_quicksort(table,i,right);
+}
+
+}
 
 void read_file(char *filename){
-  FILE *file;
-  char *original, *sorted;
-  
-  int r;
-  
-  file = fopen(filename,"r");
-  if(file == NULL){
-    printf("Cannot find %s\n",filename);
-    exit(1);
-  }
-  
-  while(1){
-    original = (char *) malloc (LINE_LENGTH);
-    r = fscanf(file, "%s\n", original);
-    
-    if(r == EOF){
-      fclose(file);
-      return;
+    FILE *file;
+    char *original, *sorted;
+
+    int r;
+
+    file = fopen(filename,"r");
+    if(file == NULL){
+        printf("Cannot find %s\n",filename);
+        exit(1);
     }
-    else { 
-      sorted = quicksort(original,0,strlen(original)-1);
-      insert_end(original,sorted);
-      // printf("TEST after insert_end %s,%s\n",original,sorted);
+
+    while(1){
+        original = (char *) malloc (LINE_LENGTH);
+        r = fscanf(file, "%s\n", original);
+
+        if(r == EOF){
+            fclose(file);
+            return;
+        }
+        else {
+            sorted = quicksort(original,0,strlen(original)-1);
+            insert_end(original,sorted);
+            //printf("TEST after insert_end %s,%s\n",original,sorted);
+        }
     }
-  }
+
 }
 
 
 
 char *find_anagram(char *input){
-  int i,n,lo,hi,mid;
+    int lo,hi,mid;
 
-  input = quicksort(input,0,strlen(input)-1);
-  
+    input = quicksort(input,0,strlen(input)-1);
+    //printf("TEST AFTER input sort %s \n",input);
 
     lo = 0;
-    hi = n-1;
-  while(lo <= hi){
-   
-    mid =(lo + hi)/2;
-    if(strcmp(input,table[mid].sorted)==0){
-      return table[mid].original;
+    hi = SIZE-1;
+
+    while(lo <= hi){
+
+        mid =(lo + hi)/2;
+        if(strcasecmp(input,table[mid].sorted)==0){
+            return table[mid].original;
+        }
+        else if(strcasecmp(input,table[mid].sorted) > 0){
+            lo = mid + 1;
+        }
+        else{
+            hi = mid - 1;
+        }
+
     }
-    else if(strcmp(input,table[mid].sorted) > 0){
-      lo = mid + 1;
-    }
-    else{
-      hi = mid - 1;
-    }
-    
-  }
-  return "could not find anagram";
+    return "could not find anagram";
 
 }
 
@@ -132,18 +171,27 @@ char *find_anagram(char *input){
 
 
 int main(void){
-  char *input, *result;
-  n = 0;
-  
-  read_file("dictionary.words");
-  //printf("TEST after read_file\n");
-  
-  printf("Enter:");
-  scanf("%s",input);
-  
-  result = find_anagram(input);
-  printf("%s\n",result);
-  
-  return 0;
-  
+    char *input, *result;
+   
+    n = 0;
+    int i = 0;
+
+    read_file("dictionary.words");
+    //printf("TEST after read_file %s\n",table[0].sorted);
+
+    table_quicksort(table,0,SIZE-1);
+    //printf("TEST AFTER table_sort \n");
+
+
+    while(i<10) {
+        printf("Enter:");
+        scanf("%s", input);
+
+        result = find_anagram(input);
+        printf("%s\n", result);
+        i++;
+    }
+        return 0;
+
 }
+
