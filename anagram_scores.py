efa3911a66630b347320b8e8b14b51962889768c
mosasaki <m.sasaki261@gmail.com>
2019-05-30 21:15:03 +0900

    added python

--- anagram_scores.py
+++ anagram_scores.py
@@ -1,104 +1,116 @@
 import sys
 from collections import Counter
-def load(file):
-    """Open a text file & turn contents into a list of lowercase strings."""
+from absl import logging
+
+
+def load_and_sort_dictionary_words(file):
+    """Open a text file & turn contents into a sorted list of lowercase strings.
+    
+    Args:
+        file (str): The file location of the dictionary.
+
+    Returns:
+        A list of sorted and lowercased dictionary words.
+
+    Raises:
+        IOError: An error occurred accessing the dictionary file.
+    """
+    
     try:
         with open(file) as in_file:
-            loaded_txt = in_file.read().strip().split('\n')
-            loaded_txt = [x.lower() for x in loaded_txt]
-            return loaded_txt
+            loaded_txt = in_file.read().strip().lower().split('\n')
+            return sorted(loaded_txt)
     except IOError as e:
-        print("{}\nError opening {}. Terminating program.".format(e, file),
-              file=sys.stderr)
-        sys.exit(1)
+        logging.fatal('Unable to open file %s',file)
+ 
 
-dict_file = load('dictionary.words')
+dict_file = load_and_sort_dictionary_words('dictionary.words')
 
-dict_file = sorted(dict_file)
 
-class anagramsInfo:
+class AnagramsInfo:
+    """A class used to represent an information(word and point) of an anagram.
+
+    Attributes:
+        word (str): A word in list of anagrams made from the input letters.  
+        point (int): A count of points of each of the words.
+
+    """
+    
     def __init__(anagram,word,point):
+        """
+        Args:
+            word (str): A word in list of anagrams made from the input letters.  
+            point (int): A count of points of each of the words.            
+        """
+
         anagram.word = word
         anagram.point = point
 
         
-def find_anagrams(mixedletters, word_list):
-    """Read name & dictionary file & display all anagrams IN mizedletters."""
-    mixed_letter_map = Counter(mixedletters)
-    anagrams = []
-    
-    
-    for word in word_list:
-        test = ''
-        word_letter_map = Counter(word.lower())
-        for letter in word:
-            if word_letter_map[letter] <= mixed_letter_map[letter]:
-                test += letter
-        if Counter(test) == word_letter_map:
-            #anagrams.append(word)
-            anagrams.append(anagramsInfo(word,0))
-    #print(*anagrams, sep='\n')
-    
+def find_anagrams(input_letters, dictionary_word_list):
+    """Read dictionary file & display all anagrams IN input_letters.
 
+    Args:
+        input_letters (str): An input from user.             
+        word_list (list): A list of sorted and lowercased dictionary words.
+  
+    """
+    
+    input_letter_map = Counter(input_letters)
+    anagrams_list = []
+      
+    for word in dictionary_word_list:
+        test = ''
+        dictionary_letter_map = Counter(word.lower())
+        for letter in word:
+            if dictionary_letter_map[letter] <= input_letter_map[letter]:
+                test += letter
+        if Counter(test) == dictionary_letter_map:
+            anagrams_list.append(AnagramsInfo(word,0))
+  
     point = 0
-    toppoint = 0
-    for word in anagrams:
-        
-        for letter in word.word:
-            
-            if letter == 'j' or letter == 'k'or letter == 'x'or letter == 'z':
+    THREE_POINT_LETTERS = frozenset(['j','k','x','z'])
+    TWO_POINT_LETTERS = frozenset(['q','c','f','h','l','m','p','v','w','y'])
+    
+    for word in anagrams_list:
+        for letter in word.word:            
+            if letter in THREE_POINT_LETTERS:
                 point += 3
-            elif letter == 'q' or letter == 'c'or letter == 'f' or letter == 'h' or letter == 'l' or letter == 'm' or letter == 'p' or letter == 'v' or letter == 'w' or letter == 'y':
+            elif letter in TWO_POINT_LETTERS:    
                 point += 2
             else:
                 point += 1
-            
-            
-       # if point > toppoint:
-        #    toppoint = point
         point = (point+1)**2
         word.point = point   
         point = 0
         
-    
-    topword = ''
-    
-    '''    
-    for word in anagrams:
-        print('word --> ' + word.word)
-        print('point --> ' + str(word.point))
-    '''        
-    for word in anagrams:
-        if word.point > toppoint:
-            toppoint = word.point
-            topword = word.word
-            
-    print('highest scored anagram --> '+ topword)
-    print('highest score --> '+ str(toppoint))
-        
-    #print("Remaining letters = {}".format(name))
-    #print("Number of remaining letters = {}".format(len(name)))
-    #print("Number of remaining (real word)anagrams = {}".format(len(anagrams)))
+    top_point = 0
+    top_word = ''
+      
+    for word in anagrams_list:
+        if word.point > top_point:
+            top_point = word.point
+            top_word = word.word
 
+    if top_word =='':
+        print('Could not find any anagrams')
+    else:
+        print('highest scored anagram --> '+ top_word)
+        print('highest score --> '+ str(top_point))
 
 
 def main():
-    """Help user build anagram phrase from their name."""
 
-    
-    running = True
     print('enter "q" to quit')
-    while running:
-        
-        ini_mixedletters = input("Enter : ")
-        #  convert to lower case
-        mixedletters = ini_mixedletters.lower()
+    
+    while True:
+        ini_input_letters = input("Enter : ")
+        input_letters = ini_input_letters.lower()  # convert to lower case
 
-        if mixedletters == "q":
-            running = False
-            sys.exit()
+        if input_letters == "q":
+            break
 
-        find_anagrams(mixedletters, dict_file)
+        find_anagrams(input_letters, dict_file)
     
 
 if __name__ == '__main__':
